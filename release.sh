a69d9a684cfc38beb7ff4b7b050aa911e28c1d9e
Anton Adamansky <adamansky@gmail.com>
2022-04-18 18:32:13 +0700

    * Docs

--- release.sh
+++ release.sh
@@ -16,9 +16,28 @@ readme() {
   cat "./BUILDING.md" >> "./README.md"
   echo -e "\n\n" >> "./README.md"
   cat "./docs/README.md" >> "./README.md"
-  echo -e '\n# License\n\n```\n' >> "./README.md"
-  cat "./LICENSE" >> "./README.md"
-  echo -e '\n```\n' >> "./README.md"
+  cat <<EOF >> "./README.md"
+#License
+
+\`\`\`
+/*
+ * Copyright (C) 2022 Greenrooms, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+\`\`\`
+EOF
 }
 
 while [ "$1" != "" ]; do
